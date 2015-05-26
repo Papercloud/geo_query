6688485304287e255de03b2ae5fcd8bdae1fe1c5
William Porter <dawilster143@gmail.com>
2015-05-26 14:03:43 +1000

    Removed reference to base_class it should be the responsibility of the user to determine scope.

--- lib/geo_query/geo_queryable.rb
+++ lib/geo_query/geo_queryable.rb
@@ -38,17 +38,18 @@ module GeoQuery
       # Class methods
       ## Returns other objects within the given radius of a point
       def self.near_lat_lng(lat, lng, radius=500) #radius in metres
-        select("#{self.base_class.table_name}.*, ST_Distance(#{self.base_class.point_column}, ST_GeographyFromText('POINT(#{lng} #{lat})')) AS distance")
-        .where("ST_DWithin(#{self.base_class.point_column} , ST_GeographyFromText('POINT(#{lng} #{lat})'), #{radius})")
+        select("#{self.table_name}.*, ST_Distance(#{self.point_column}, ST_GeographyFromText('POINT(#{lng} #{lat})')) AS distance")
+        .where("ST_DWithin(#{self.point_column} , ST_GeographyFromText('POINT(#{lng} #{lat})'), #{radius})")
         .order("distance ASC")
       end
 
       ## Returns all objects within a bounding box
       def self.within_bounding_box(min_lat, min_lng, max_lat, max_lng)
-        select("#{self.base_class.table_name}.*").
-        where("#{self.base_class.point_column} && ST_MakeEnvelope(?, ?, ?, ?, 4326)",
+        select("#{self.table_name}.*").
+        where("#{self.point_column} && ST_MakeEnvelope(?, ?, ?, ?, 4326)",
           min_lng, min_lat, max_lng, max_lat)
       end
+
     end
 
     module ClassMethods
